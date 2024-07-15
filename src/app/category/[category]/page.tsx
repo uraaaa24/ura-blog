e2d490a5e3548a884482570d3097a67f9841f365
Yuto Urashima <yuto.urashima24@gmail.com>
2024-07-15 09:41:36 +0900

    feat: カテゴリーページにパンくずリストを追加

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -1,3 +1,4 @@
+import Breadcrumb from '@/component/element/breadcrumb'
 import CardList from '@/component/element/cardList'
 import TagSidebar from '@/component/element/sideBar/tagsSidebar'
 import { getAllTags, getArticlesByTag } from '@/util/microcms'
@@ -21,9 +22,13 @@ const Category = async ({
 
   const tags = await getAllTags()
 
+  // TODO: labelがidのままなので、名前が表示されるように修正する
+  const breadcrumbItems = [{ label: category, href: `/category/${category}` }]
+
   return (
     <div className="flex gap-8">
-      <div className="w-3/4">
+      <div className="flex flex-col gap-1 w-3/4">
+        <Breadcrumb items={breadcrumbItems} />
         <CardList articleList={articles} />
       </div>
       <aside className="w-1/4">
--- src/component/element/breadcrumb/index.tsx
+++ src/component/element/breadcrumb/index.tsx
@@ -1,19 +1,40 @@
+import { ChevronRight } from 'lucide-react'
+import Link from 'next/link'
+
+type BreadcrumbItem = {
+  label: string
+  href: string
+}
+
 type BreadcrumbProps = {
-  words: string[]
+  items: BreadcrumbItem[]
 }
 
 const Breadcrumb = (props: BreadcrumbProps) => {
+  const breadcrumbItems = [{ label: 'Home', href: '/articles' }, ...props.items]
+
   return (
-    // textは上下中央寄せ
-    <div className="text-center">
-      {props.words.map((word) => {
-        return (
-          <span key={word} className="text-sm border-b border-gray-300 pb-2">
-            {word}
-          </span>
-        )
-      })}
-    </div>
+    <nav>
+      <ul className="flex items-center gap-1 break-words text-sm">
+        {breadcrumbItems.map((item, index) => {
+          return (
+            <li key={index}>
+              {/* itemsの最後は、hrefをつけない */}
+              {index === breadcrumbItems.length - 1 ? (
+                item.label
+              ) : (
+                <div className="flex gap-1 items-center">
+                  <Link href={item.href} className="hover:text-[#e30613] transition-all duration-300">
+                    {item.label}
+                  </Link>
+                  <ChevronRight size={16} />
+                </div>
+              )}
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
   )
 }
 
