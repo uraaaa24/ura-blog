ec0d1762c2c7642dfc211386962dae299cb077ed
Yuto Urashima <yuto.urashima24@gmail.com>
2024-07-24 15:27:28 +0900

    feat: ページデザインを微調整

--- src/app/(article)/articles/[id]/page.tsx
+++ src/app/(article)/articles/[id]/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 import RichEditor from '@/component/element/richEditor'
 import TableOfContentsSideBar from '@/component/element/sideBar/tableOfContentsSidebar'
 import CategoryTip from '@/component/element/tip/categoryTip'
@@ -20,32 +18,20 @@ const Article = async ({
     <article className="rounded-sm flex flex-col gap-6">
       <div className="flex w-full gap-6">
         <div className="w-3/4 flex flex-col bg-white py-6 px-10">
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-col gap-1.5 mb-2">
-              <p className="text-gray-500">{convertDate(data.createdAt)}</p>
-              <h1 className="text-4xl font-bold">{data.title}</h1>
-              {data.tags && (
-                <div className="flex gap-1">
-                  {data.tags.map((tag, index) => (
-                    <CategoryTip key={index} id={tag.id} name={tag.name} />
-                  ))}
-                </div>
-              )}
-            </div>
-            <Image
-              src={data.eyeCatch?.url ?? '/static/no-image.png'}
-              alt="アイキャッチ"
-              width={data.eyeCatch?.width ?? 600}
-              height={data.eyeCatch?.height ?? 450}
-              style={{ maxHeight: 400, width: '100%', objectFit: 'cover' }}
-              loading="eager"
-              priority
-            />
+          <div className="flex flex-col gap-1.5 mb-2 text-center">
+            <p className="text-gray-500">{convertDate(data.createdAt)}</p>
+            <h1 className="text-4xl font-bold">{data.title}</h1>
+            {data.tags && (
+              <div className="flex gap-1 justify-center mt-1">
+                {data.tags.map((tag, index) => (
+                  <CategoryTip key={index} id={tag.id} name={tag.name} />
+                ))}
+              </div>
+            )}
           </div>
           <RichEditor body={data.body} />
         </div>
         <aside className="w-1/4">
-          {/* TODO: 目次用のサイドバーを作成する */}
           <TableOfContentsSideBar />
         </aside>
       </div>
