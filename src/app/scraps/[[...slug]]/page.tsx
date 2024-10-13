4eab99a4334e920b235a89a05766ccd993de3d5a
Yuto Urashima <yuto.urashima24@gmail.com>
2024-10-13 19:00:13 +0900

    chore: update .gitignore and package.json

---
+++ src/app/scraps/[[...slug]]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ScrapPage = ({ params }: { params: { slug: string[] } }) => {
+  console.log(params)
+  return <div>ScrapPage</div>
+}
+
+export default ScrapPage
---
+++ src/app/scraps/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ScrapsPage = () => {
+  return <div>ScrapsPage</div>
+}
+
+export default ScrapsPage
