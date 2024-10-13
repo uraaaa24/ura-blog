7842f27f7f648316d424e4044cd0318ad0369fe1
Yuto Urashima <yuto.urashima24@gmail.com>
2024-10-13 18:16:05 +0900

    chore: update code formatting and linting configuration

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// TODO: ページ構成がはっきりしたら、ヘッダーのデザインを考える
+const Header = () => {
+  return (
+    <div className="flex h-16 w-full items-center bg-green-400">
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header
