dfc8edf2b33f47396d7a6a1be9d2e5fb49c42ace
Yuto Urashima <yuto.urashima24@gmail.com>
2024-11-01 12:09:34 +0900

    refactor: update styling in ArticlesPage component

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -9,7 +9,7 @@ const ArticlePage = ({ params }: { params: { slug: string } }) => {
   const { title, date, tags, content } = getPostBySlug('article', params.slug)
 
   return (
-    <article className="rounded-xl bg-white px-8 py-10 dark:bg-gray-800">
+    <article className="my-8 rounded-xl border-2 border-[#e5e5e5] p-10 dark:border-[#37464f] ">
       <header className="mb-10 flex flex-col items-center gap-2 text-center">
         <p className="text-gray-500 dark:text-gray-400">{date}</p>
         <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-100">{title}</h1>
