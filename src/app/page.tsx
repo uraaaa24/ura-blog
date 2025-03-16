2152ccdea1c1b85ea50cc34a6e39f29aa0464a41
Yuto Urashima <yuto.urashima24@gmail.com>
2025-03-17 02:08:24 +0900

    feat: データ取得ロジックを統合し、fetchPosts関数を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 import Section from '@/components/section'
-import { getAllPosts } from '@/lib/post'
-import { getZennRssFeed } from '@/lib/zenn'
+import { fetchPosts } from '@/lib/data'
 
 import HeroContent from './_components/hero-content'
 import LatestPosts from './_components/latest-posts'
 
 const HomePage = async () => {
-  const posts = await getAllPosts()
-  const zennPosts = await getZennRssFeed()
-
-  const totalPosts = Array.from(new Set([...posts, ...zennPosts]))
-    .slice(0, 5)
-    .sort((a, b) => new Date(b.formattedDate).getTime() - new Date(a.formattedDate).getTime())
+  const totalPosts = await fetchPosts(5)
 
   return (
     <>
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,15 +1,9 @@
 import { Heading1 } from '@/components/heading'
 import PostItems from '@/components/posts/post-items'
-import { getAllPosts } from '@/lib/post'
-import { getZennRssFeed } from '@/lib/zenn'
+import { fetchPosts } from '@/lib/data'
 
 const PostsPage = async () => {
-  const posts = await getAllPosts()
-  const zennPosts = await getZennRssFeed()
-
-  const totalPosts = Array.from(new Set([...posts, ...zennPosts])).sort(
-    (a, b) => new Date(b.formattedDate).getTime() - new Date(a.formattedDate).getTime()
-  )
+  const totalPosts = await fetchPosts()
 
   return (
     <>
