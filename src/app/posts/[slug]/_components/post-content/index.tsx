0957d785880d6c4967a2a85ade1d9108ed510efb
Yuto Urashima <yuto.urashima24@gmail.com>
2025-03-17 02:13:07 +0900

    feat: PostContentコンポーネントのMarkdownレンダリングを改善し、カスタムコンポーネントを統合

--- src/app/posts/[slug]/_components/post-content/index.tsx
+++ src/app/posts/[slug]/_components/post-content/index.tsx
@@ -23,44 +23,50 @@ import {
   MDTableRow
 } from './md-parts/md-table'
 
+import type { Components } from 'react-markdown'
+
+const components: Components | null | undefined = {
+  code: ({ className, children, ...props }) => (
+    <MDCodeBlock className={className} {...props}>
+      {children}
+    </MDCodeBlock>
+  ),
+  h2: ({ children }) => <MDHeading2>{children}</MDHeading2>,
+  h3: ({ children }) => <MDHeading3>{children}</MDHeading3>,
+  p: ({ children }) => <MDParagraph>{children}</MDParagraph>,
+  table: ({ children }) => <MDTable>{children}</MDTable>,
+  tbody: ({ children }) => <MDTableBody>{children}</MDTableBody>,
+  tr: ({ children }) => <MDTableRow>{children}</MDTableRow>,
+  thead: ({ children }) => <MDTableHead>{children}</MDTableHead>,
+  th: ({ children }) => <MDTableHeadCell>{children}</MDTableHeadCell>,
+  td: ({ children }) => <MDTableCell>{children}</MDTableCell>,
+  ul: ({ children }) => <MDUnorderedList>{children}</MDUnorderedList>,
+  ol: ({ children }) => <MDOrderedList>{children}</MDOrderedList>,
+  li: ({ children }) => <MDListItem>{children}</MDListItem>,
+  img: ({ src, alt }) => <MDImage src={src} alt={alt} />,
+  hr: () => <MDHorizontalRule />,
+  a: ({ children, href, ...props }) => (
+    <MDAnchor href={href} {...props}>
+      {children}
+    </MDAnchor>
+  ),
+  blockquote: ({ children }) => <MDBlockquote>{children}</MDBlockquote>
+}
+
 type PostContentProps = {
   content: string
 }
 
-export default function PostContent({ content }: PostContentProps) {
+const PostContent = ({ content }: PostContentProps) => {
   return (
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeRaw, rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]]}
-      components={{
-        code: ({ className, children, ...props }) => (
-          <MDCodeBlock className={className} {...props}>
-            {children}
-          </MDCodeBlock>
-        ),
-        h2: ({ children }) => <MDHeading2>{children}</MDHeading2>,
-        h3: ({ children }) => <MDHeading3>{children}</MDHeading3>,
-        p: ({ children }) => <MDParagraph>{children}</MDParagraph>,
-        table: ({ children }) => <MDTable>{children}</MDTable>,
-        tbody: ({ children }) => <MDTableBody>{children}</MDTableBody>,
-        tr: ({ children }) => <MDTableRow>{children}</MDTableRow>,
-        thead: ({ children }) => <MDTableHead>{children}</MDTableHead>,
-        th: ({ children }) => <MDTableHeadCell>{children}</MDTableHeadCell>,
-        td: ({ children }) => <MDTableCell>{children}</MDTableCell>,
-        ul: ({ children }) => <MDUnorderedList>{children}</MDUnorderedList>,
-        ol: ({ children }) => <MDOrderedList>{children}</MDOrderedList>,
-        li: ({ children }) => <MDListItem>{children}</MDListItem>,
-        img: ({ src, alt }) => <MDImage src={src} alt={alt} />,
-        hr: () => <MDHorizontalRule />,
-        a: ({ children, href, ...props }) => (
-          <MDAnchor href={href} {...props}>
-            {children}
-          </MDAnchor>
-        ),
-        blockquote: ({ children }) => <MDBlockquote>{children}</MDBlockquote>
-      }}
+      components={components}
     >
       {content}
     </ReactMarkdown>
   )
 }
+
+export default PostContent
