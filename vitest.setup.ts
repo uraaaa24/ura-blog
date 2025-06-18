f55cf12beb691c831e6ac7e6ca6be6f0b4202604
Yuto Urashima <yuto.urashima24@gmail.com>
2025-06-19 07:23:05 +0900

    fix: eslintの警告を無視するコメントを追加

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -5,6 +5,7 @@ import { vi } from 'vitest'
 
 // Mock Next.js Image component
 vi.mock('next/image', () => ({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   default: ({ src, alt, ...props }: any) => {
     return React.createElement('img', { src, alt, ...props })
   }
