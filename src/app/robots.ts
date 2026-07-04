f5daaddf205d3a22f7a6c53eddf6a6d5eb12e155
Yuto Urashima <yuto.urashima24@gmail.com>
2026-07-04 13:40:29 +0900

    refactor: update robots.txt to disallow only /api/ path

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/', '/_next/']
+      disallow: ['/api/']
     },
     sitemap: `${BASE_URL}/sitemap.xml`
   }
